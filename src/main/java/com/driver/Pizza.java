5a5e6557a3935498c764e59e0057471e7b07b100
SachinMulgir <smulgir636@gmail.com>
2023-04-14 23:31:26 +0530

    updated all files of pizza and deluxe pizza

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,38 @@ package com.driver;
 
 public class Pizza {
 
+    /*
+    Veg pizza base price = 300
+    Non-veg pizza base price = 400
+    Extra Cheese Price = 80
+    Extra Toppings For Veg Pizza = 70
+    Extra Toppings For Non-veg Pizza = 120
+    Paper bag Price = 20
+     */
     private int price;
+    private int paperBag;
+    private int basePrice;
+    private int toppings;
+    private int cheesePrice = 80;
+    private int vegTopping = 70;
+    private int nonvegTopping = 120;
+    private int takeaway = 20;
+
     private Boolean isVeg;
+    private boolean isCheeseAdded;
+    private boolean isToppingAdded;
+    private boolean isTakeaway;
+
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        basePrice = isVeg ? 300 : 400;
+        this.price = basePrice;
+        paperBag = 20;
+        this.isCheeseAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeaway = false;
     }
 
     public int getPrice(){
@@ -16,19 +41,46 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if( !isCheeseAdded ){
+            isCheeseAdded = true;
+            price += cheesePrice;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if( !isToppingAdded ){
+            isToppingAdded = true;
+            price += isVeg ? vegTopping : nonvegTopping;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if( !isTakeaway ){
+            price += takeaway;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        String tempBill = "";
+        tempBill += "Base Price Of The Pizza: " + basePrice + "\n";
+        if( isCheeseAdded ){
+            tempBill += "Extra Cheese Added: " + cheesePrice + "\n";
+        }
+        if( isToppingAdded ){
+            tempBill += "Extra Toppings Added: " + toppings + "\n";
+        }
+        if( isTakeaway ){
+            tempBill += "Paperbag Added: " + paperBag + "\n";
+        }
+        tempBill += "Total Price: " + price + "\n";
+        this.bill = tempBill;
         return this.bill;
     }
 }
+/*
+Base Price Of The Pizza: 300
+Extra Cheese Added: 80
+Extra Toppings Added: 70
+Paperbag Added: 20
+Total Price: 470
+ */
